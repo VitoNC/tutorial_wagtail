07a97eb460ec6108003309a7253ed8ac26a330c0
VitoNC <91590623+VitoNC@users.noreply.github.com>
2022-02-22 17:13:27 +0100

    dev

--- blogclase/settings/dev.py
+++ blogclase/settings/dev.py
@@ -7,11 +7,7 @@ DEBUG = True
 SECRET_KEY = 'django-insecure-ljokrzvvwsydc&2d+_d6wd+t#eo2+t*ie3!cnt4ksdw!me9*%i'
 
 # SECURITY WARNING: define the correct hosts in production!
-<<<<<<< HEAD
 ALLOWED_HOSTS = ['*','192.168.1.78'] 
-=======
-ALLOWED_HOSTS = ['*','10.6.9.156'] 
->>>>>>> trabajo
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
